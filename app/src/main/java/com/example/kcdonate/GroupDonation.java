ad010b42a3c27aa0b9e3d2ea6fc5c3bb91a5f3df
TN21 <37962244+TN21@users.noreply.github.com>
2019-02-27 22:02:44 -0600

    Update GroupDonation.java

--- app/src/main/java/com/example/kcdonate/GroupDonation.java
+++ app/src/main/java/com/example/kcdonate/GroupDonation.java
@@ -2,12 +2,21 @@ package com.example.kcdonate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class GroupDonation extends AppCompatActivity {
+    ListView listView;
+
+    String[] groups = {"KC Hospital Blood Denotation", "KC Pet Rescue","Community Kitchen", "City Shelter"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_donation);
+        listView =  findViewById(R.id.gourpDonationListView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,groups);
+        listView.setAdapter(arrayAdapter);
+
     }
 }
