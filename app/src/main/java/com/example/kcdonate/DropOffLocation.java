f12bc8157780ac526e87ae597053438080962beb
TN21 <609461819Tan>
2019-03-13 00:10:04 -0500

    google maps intent

--- app/src/main/java/com/example/kcdonate/DropOffLocation.java
+++ app/src/main/java/com/example/kcdonate/DropOffLocation.java
@@ -1,13 +1,35 @@
 package com.example.kcdonate;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DropOffLocation extends AppCompatActivity {
+
+    public Button dropOffLocBtn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drop_off_location);
+
+        dropOffLocBtn1 = findViewById(R.id.dropOffLocBtn1);
+
+        dropOffLocBtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String uri = "https://maps.google.com/maps?f=d&daddr=9107, MO-45, Parkville, MO 64152";
+                Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(uri));
+                intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+                startActivity(intent);
+
+            }
+        });
+
+
+
     }
 }
