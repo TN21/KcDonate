5be2a3d1314768d7f32bcf7ec79d7e3ea5d463e2
TN21 <609461819Tan>
2019-04-25 15:03:00 -0500

    update

--- app/src/main/java/com/example/kcdonate/ContactUs.java
+++ app/src/main/java/com/example/kcdonate/ContactUs.java
@@ -1,13 +1,46 @@
 package com.example.kcdonate;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ContactUs extends AppCompatActivity {
+
+    private Button callBtn;
+    private Button emailBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+
+        callBtn = findViewById(R.id.callBtn);
+        emailBtn = findViewById(R.id.emailBtn);
+
+        callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:8163724777"));
+                startActivity(intent);
+
+            }
+        });
+
+        emailBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("plain/text");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[] { "kcdonate@gmail.com" });
+                intent.putExtra(Intent.EXTRA_SUBJECT, "");
+                intent.putExtra(Intent.EXTRA_TEXT, "");
+                startActivity(Intent.createChooser(intent, ""));
+
+            }
+        });
     }
 }
